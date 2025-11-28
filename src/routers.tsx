b500949159e33e4c75a46b23fa4fba7115cafcf2
Meawjai <wananuthida.46@gmail.com>
2025-11-28 18:55:32 +0700

    commit by dawan

--- src/routers.tsx
+++ src/routers.tsx
@@ -4,6 +4,7 @@ import Login from "./pages/main/login";
 import Register from "./pages/main/register";
 import Homepage from "./pages/main/homepage";
 import InvoiceConfirm from "./pages/invoice/invoice_confirm";
+import InvoiceConfirmReport from "./pages/invoice/invoice_confirmrp";
 
 function Routers() {
 
@@ -16,7 +17,7 @@ function Routers() {
                     <Route path="register" element={<Register />} />
                     <Route path="homepage" element={<Homepage />} />
                     <Route path="invconfrim" element={<InvoiceConfirm/>}/>
-                    <Route path="invconfrimrp" element={<InvoiceConfirm/>}/>
+                    <Route path="invconfrimrp" element={<InvoiceConfirmReport/>}/>
                 </Route>
             </Routes>
         </Router>
