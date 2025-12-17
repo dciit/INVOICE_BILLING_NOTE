71243b5e981de0eec765d0ccb3c62dead3a055db
Meawjai <wananuthida.46@gmail.com>
2025-12-17 09:08:25 +0700

    commit by dawan

--- src/components/main/pagemenu.tsx
+++ src/components/main/pagemenu.tsx
@@ -1,35 +1,13 @@
 import { Menu, type MenuProps } from "antd";
 import { CalendarOutlined, FileProtectOutlined, FileTextOutlined, FormOutlined } from "@ant-design/icons";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 type MenuItem = Required<MenuProps>["items"][number];
 
 const withMenuIcon = (Icon: React.ElementType) => {
     return <span className="text-sm font-bold text-white"><Icon /></span>
 };
-
-const menuItems: MenuItem[] = [
-    {
-        key: `/invconfrim`,
-        label: 'Confirm Invoice',
-        icon: withMenuIcon(FormOutlined),
-    },
-    {
-        key: `/invconfrimrp`,
-        label: 'Confirm Invoice Report',
-        icon: withMenuIcon(FileProtectOutlined),
-    },
-    {
-        key: `/calendarbulling`,
-        label: 'Calendar Bulling Note',
-        icon: withMenuIcon(CalendarOutlined)
-    },
-    {
-        key: `/manual`,
-        label: 'Manual',
-        icon: withMenuIcon(FileTextOutlined)
-    }
-];
 
 interface PageMenuProps {
     onCloseDrawer: () => void;
@@ -38,6 +16,34 @@ interface PageMenuProps {
 const PageMenu = ({ onCloseDrawer }: PageMenuProps) => {
     const location = useLocation();
     const navigate = useNavigate();
+
+    const auth = useSelector((state: any) => state.reducer.authen);
+
+    const menuItems: MenuItem[] = [
+        {
+            key: `/invconfrim`,
+            label: 'Confirm Invoice',
+            icon: withMenuIcon(FormOutlined),
+        },
+        {
+            key: `/invconfrimrp`,
+            label: 'Confirm Invoice Report',
+            icon: withMenuIcon(FileProtectOutlined),
+        },
+        {
+            key: `/calendarbulling`,
+            label: 'Calendar Bulling Note',
+            icon: withMenuIcon(CalendarOutlined)
+        },
+        ...(auth?.role === "rol_accountant"
+            ? [{
+                key: `/ReportAC`,
+                label: 'Report AC',
+                icon: withMenuIcon(FileProtectOutlined),
+            }]
+            : []
+        )
+    ];
 
     const handleClick: MenuProps['onClick'] = (e) => {
         navigate(e.key);
